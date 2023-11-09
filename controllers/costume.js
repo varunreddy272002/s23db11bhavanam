58f6c737020e9d09ef39d8e05f1d4274ca1c348d
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-11-08 21:15:18 -0600

    added puppy details

--- controllers/costume.js
+++ controllers/costume.js
@@ -30,9 +30,26 @@ exports.costume_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.costume_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume create POST');
-};
+exports.costume_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Costume();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.costume_type = req.body.costume_type;
+    document.cost = req.body.cost;
+    document.size = req.body.size;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
 // Handle Costume delete form on DELETE.
 exports.costume_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
