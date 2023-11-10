451713399dfce0b247d0c03bdeec721206ab3999
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-11-09 19:45:53 -0600

    done with get request id

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -26,9 +26,19 @@ exports.costume_view_all_Page = async function(req, res) {
 
 
 // for a specific Costume.
-exports.costume_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
-};
+exports.costume_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Costume.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
+
+
 // Handle Costume create on POST.
 exports.costume_create_post = async function(req, res) {
     console.log(req.body)
