14e384cdc3bf3e3579347d247602a93b22de3ebe
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-11-09 21:42:03 -0600

    added put

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -65,6 +65,22 @@ exports.costume_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.costume_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
+exports.costume_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Costume.findById( req.params.id)
+// Do updates of properties
+if(req.body.costume_type)
+toUpdate.costume_type = req.body.costume_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.size) toUpdate.size = req.body.size;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
