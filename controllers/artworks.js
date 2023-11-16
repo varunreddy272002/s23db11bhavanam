838ab82ad7fc08e4ee64bad19b10569af571ed9b
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-11-15 18:30:00 -0600

    created new instance

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -108,3 +108,17 @@ exports.costume_view_one_Page = async function (req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.costume_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('artworkcreate', { title: 'ArtWork Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/artworks.js
+++ routes/artworks.js
@@ -7,5 +7,9 @@ router.get('/', costume_controlers.costume_view_all_Page);
 
 /* GET detail costume page */
 router.get('/detail', costume_controlers.costume_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', costume_controlers.costume_create_Page);
+
  
 module.exports = router;
