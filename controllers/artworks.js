a0d934620ad478976daa33f07197ebec26539f12
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-11-15 18:18:47 -0600

    completed view one page by id

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -1,5 +1,5 @@
 var Costume = require('../models/artworks');
-
+var ObjectId = require('mongodb').ObjectId;
 // List of all Costumes
 exports.costume_list = async function (req, res) {
     try {
@@ -13,34 +13,34 @@ exports.costume_list = async function (req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.costume_view_all_Page = async function(req, res) {
-    try{
-    results = await Costume.find();
-    res.render('artworks', { title: 'Costume Search Results', results: results });
+exports.costume_view_all_Page = async function (req, res) {
+    try {
+        results = await Costume.find();
+        res.render('artworks', { title: 'Costume Search Results', results: results });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
+};
 
 
 // for a specific Costume.
-exports.costume_detail = async function(req, res) {
+exports.costume_detail = async function (req, res) {
     console.log("detail" + req.params.id)
     try {
-    result = await Costume.findById( req.params.id)
-    res.send(result)
+        result = await Costume.findById(req.params.id)
+        res.send(result)
     } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
     }
-    };
-    
+};
+
 
 
 // Handle Costume create on POST.
-exports.costume_create_post = async function(req, res) {
+exports.costume_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Costume();
     // We are looking for a body, since POST does not have query parameters.
@@ -50,37 +50,61 @@ exports.costume_create_post = async function(req, res) {
     document.costume_type = req.body.costume_type;
     document.cost = req.body.cost;
     document.size = req.body.size;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-    
-// Handle Costume delete form on DELETE.
-exports.costume_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
+
+// Handle Costume delete on DELETE.
+exports.costume_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Costume.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
 // Handle Costume update form on PUT.
-exports.costume_update_put = async function(req, res) {
-console.log(`update on id ${req.params.id} with body
+exports.costume_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
-try {
-let toUpdate = await Costume.findById( req.params.id)
-// Do updates of properties
-if(req.body.costume_type)
-toUpdate.costume_type = req.body.costume_type;
-if(req.body.cost) toUpdate.cost = req.body.cost;
-if(req.body.size) toUpdate.size = req.body.size;
-let result = await toUpdate.save();
-console.log("Sucess " + result)
-res.send(result)
-} catch (err) {
-res.status(500)
-res.send(`{"error": ${err}: Update for id ${req.params.id}
+    try {
+        let toUpdate = await Costume.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.costume_type)
+            toUpdate.costume_type = req.body.costume_type;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        if (req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
 failed`);
-}
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.costume_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        const id = new ObjectId(req.query.id);
+        result = await Costume.findById(id)
+        res.render('artworkdetail',
+            { title: 'ArtWork Detail', result: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
 };
--- routes/artworks.js
+++ routes/artworks.js
@@ -4,5 +4,8 @@ var router = express.Router();
  
 /* GET costumes view */
 router.get('/', costume_controlers.costume_view_all_Page);
+
+/* GET detail costume page */
+router.get('/detail', costume_controlers.costume_view_one_Page);
  
 module.exports = router;
