7c02ffd7536eae9b0be4364864983dab3f6b1b80
varunreddy272002 <143002746+varunreddy272002@users.noreply.github.com>
2023-11-29 20:35:49 -0600

    secured update page

--- routes/artworks.js
+++ routes/artworks.js
@@ -2,6 +2,13 @@ var express = require('express');
 const costume_controlers = require('../controllers/artworks');
 var router = express.Router();
  
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+
 /* GET costumes view */
 router.get('/', costume_controlers.costume_view_all_Page);
 
@@ -12,7 +19,7 @@ router.get('/detail', costume_controlers.costume_view_one_Page);
 router.get('/create', costume_controlers.costume_create_Page);
 
 /* GET create update page */
-router.get('/update', costume_controlers.costume_update_Page);
+router.get('/update', secured, costume_controlers.costume_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', costume_controlers.costume_delete_Page);
